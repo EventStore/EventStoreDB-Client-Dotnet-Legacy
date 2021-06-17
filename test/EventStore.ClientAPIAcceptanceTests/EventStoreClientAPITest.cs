197d11412f8836682755dce95c3a3b2663c9f2d9
Timothy Coleman <timothy.coleman@gmail.com>
2021-06-17 16:27:12 +0100

    Swap EventStoreClientAPITest <-> EventStoreClientAPIClusterTest
    They were back to front in the files

--- test/EventStore.ClientAPIAcceptanceTests/EventStoreClientAPITest.cs
+++ test/EventStore.ClientAPIAcceptanceTests/EventStoreClientAPITest.cs
@@ -1,8 +1,25 @@
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Xunit;
 
 namespace EventStore.ClientAPI {
 	[Collection(nameof(EventStoreClientAPICollection))]
-	public abstract class EventStoreClientAPIClusterTest : IClassFixture<EventStoreClientAPIClusterFixture> {
+	public abstract class EventStoreClientAPITest : IClassFixture<EventStoreClientAPIFixture> {
+		protected string GetStreamName([CallerMemberName] string testMethod = default)
+			=> $"{GetType().Name}_{testMethod ?? "unknown"}";
+
+		protected static IEnumerable<bool> UseSsl => new[] { true };
+
+		protected static IEnumerable<(long expectedVersion, string displayName)> ExpectedVersions
+			=> new[] {
+				((long)ExpectedVersion.Any, nameof(ExpectedVersion.Any)),
+				(ExpectedVersion.NoStream, nameof(ExpectedVersion.NoStream))
+			};
+
+		public static IEnumerable<object[]> ExpectedVersionTestCases() {
+			foreach (var (expectedVersion, displayName) in ExpectedVersions) {
+				yield return new object[] { expectedVersion, displayName };
+			}
+		}
 	}
 }
--- test/EventStore.ClientAPIAcceptanceTests/cluster/EventStoreClientAPIClusterTest.cs
+++ test/EventStore.ClientAPIAcceptanceTests/cluster/EventStoreClientAPIClusterTest.cs
@@ -1,25 +1,7 @@
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using Xunit;
 
 namespace EventStore.ClientAPI {
 	[Collection(nameof(EventStoreClientAPICollection))]
-	public abstract class EventStoreClientAPITest : IClassFixture<EventStoreClientAPIFixture> {
-		protected string GetStreamName([CallerMemberName] string testMethod = default)
-			=> $"{GetType().Name}_{testMethod ?? "unknown"}";
-
-		protected static IEnumerable<bool> UseSsl => new[] {true};
-
-		protected static IEnumerable<(long expectedVersion, string displayName)> ExpectedVersions
-			=> new[] {
-				((long)ExpectedVersion.Any, nameof(ExpectedVersion.Any)),
-				(ExpectedVersion.NoStream, nameof(ExpectedVersion.NoStream))
-			};
-
-		public static IEnumerable<object[]> ExpectedVersionTestCases() {
-			foreach (var (expectedVersion, displayName) in ExpectedVersions) {
-				yield return new object[] {expectedVersion, displayName};
-			}
-		}
+	public abstract class EventStoreClientAPIClusterTest : IClassFixture<EventStoreClientAPIClusterFixture> {
 	}
 }
